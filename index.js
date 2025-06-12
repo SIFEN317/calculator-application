b4097f40b190c9884f3306bf8310710b28e5c46f
Sifen Abduselam <sifenabduselam7@gmail.com>
2025-06-12 12:11:27 -0700

    update on the javascript

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 const display = document.getElementById('display')
 
-function appendToDisplay(input){
 
-    display.value += input
+  document.querySelectorAll('[data-number]').forEach(button => {
+    button.addEventListener('click',() => {
+        display.value += button.dataset.number})
 
-}
 
-function clearDisplay(){
-display.value = "";
-}
+let firstNumber = null;
+let operator = null;
+// parseFloat used to change the string to number
+document.getElementById('plus').addEventListener
+('click', () => {
+    firstNumber = parseFloat(display.value);
+    operator = '+';
+    display.value = '';
+});
 
-function calculate(){ 
-    try{
-        display.value = eval(display.value)
+document.getElementById('equals').addEventListener
+('click', () => {
+    if(firstNumber !== null && operator === '+') {
+        const secondNumber = parseFloat(display.value);
+        const result = firstNumber + secondNumber;
+        display.value = result;
+        firstNumber = null;
     }
-catch(error) {
-display.value = "Error"
-}
-}
+})
+
+,
+
+document.getElementById('clear').addEventListener
+('click', () => {
+    display.value ='';
+    firstNumber = null;
+    operator = null;
+
+})
+// then the clear button is clicked display value will be emplity and also first number and operator will be empity 
+
+
+
+
+
 
